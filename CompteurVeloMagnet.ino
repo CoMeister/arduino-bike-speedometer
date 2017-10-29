2bbcbd50f3221b62af83814a6db695879e342e8a
CoMeister <corneille.braillard@gmail.com>
2017-10-29 18:00:09 +0100

    Update 1.1

--- CompteurVeloMagnet.ino
+++ CompteurVeloMagnet.ino
@@ -1,11 +1,19 @@
+/*List composant:
+ * Arduino uno or Atmega328P-PU
+ * If you use an Atmega328P-PU add 1 16Mhz quartz and two ceramic condensator 22 Pn
+ * MAX7219 or MAX7221
+ * 3 7segment digit anode common
+ * magnet sensor A3144E 
+ */
+
 #include "LedControl.h"
-LedControl lc = LedControl(12,11,10,3);  //DIN, CLK, CS, nombre de 7 segments
+LedControl lc = LedControl(12, 11, 10, 1); //DIN, CLK, CS, number of 7 segments
 int magnetSensor = 2;
 
-int inchDiametre = 26;
-double cmDiametre = 0;
+int inchDiameter = 26;    //!!! enter inch of your wheel diameter !!!//
+double cmDiameter = 0;
 
-double cmPourtour = 0;
+double cmPerimeter = 0;
 
 double timeLastCheck = 0;
 
@@ -14,67 +22,46 @@ double speedMeterSecond = 0.0;
 double speedKilometerHour = 0.0;
 
 void setup() {
-  lc.shutdown(0,false); //0 le nombre qui defini l'emplacement si il y a un chainage de max7219
-  lc.setIntensity(0,2);
+  lc.shutdown(0, false); //0 number of max7219 cascading
+  lc.setIntensity(0, 8);
   lc.clearDisplay(0);
-  lc.setDigit(0,0,'-', false);
-  lc.setDigit(0,1,'-', false);
+  lc.setDigit(0, 0, 0, false);
+  lc.setDigit(0, 1, 0, false);
+  lc.setDigit(0, 2, 0, false);
   pinMode(magnetSensor, INPUT);
   Serial.begin(9600);
-  cmDiametre = (double)inchDiametre/0.39370;
-  //Serial.println(cmDiametre);
-  cmPourtour = (double)PI * cmDiametre;
-  //Serial.println(cmPourtour);
+  cmDiameter = (double)inchDiameter / 0.39370;    // inch to centimeter
+  //Serial.println(cmDiameter);
+  cmPerimeter = (double)PI * cmDiameter;
+  //Serial.println(cmPerimeter);
   timeLastCheck = millis();
-  pinMode(13, OUTPUT);
-  digitalWrite(13, 1);
-  writeNumber(123);
-  delay(500);
-  for(int i = 10; i > 0; i--){
-    writeNumber(i);
-    delay(250);
-  }
   writeNumber(0);
-  delay(2000);
+  delay(100);
 }
 
 void loop() {
-  if(digitalRead(magnetSensor) == 0 && ((double)millis()-timeLastCheck*1000) > 5){
-    //Serial.println((double)millis()/1000-timeLastCheck);
-    speedMeterSecond = (cmPourtour/100)/((double)millis()/1000-timeLastCheck);
-    timeLastCheck = (double)millis()/1000;
-    //speedMeterSecond = speedCmPerCentSec/100;
-    speedKilometerHour = speedMeterSecond*3.6;
+  if (digitalRead(magnetSensor) == 0) { // &&
+    speedMeterSecond = (cmPerimeter / 100) / ((double)millis() / 1000 - timeLastCheck); // m/s = metre/(tempActuel(seconde)-dernierTmpEnregistré(seconde))
+    timeLastCheck = (double)millis() / 1000;
+    speedKilometerHour = speedMeterSecond * 3.6;
     writeNumber((int)speedKilometerHour);
-    digitalWrite(13, 1);
-    delay(10);
-  }else{
-    if(millis() % 500 == 0){
-      digitalWrite(13, 0);
+  } else {
+    if (((double)millis() / 1000 - timeLastCheck) >= 5) {
+      writeNumber(0);
     }
   }
 
-  if((double)millis()/1000-timeLastCheck > 2){
-    writeNumber(0);
-  }
+  delay(15);
 }
 
-void writeNumber(int number){
+void writeNumber(int number) {  //max 999 | 1342 -> 342
   int digit[] = {0, 0, 0};
-  int stat = 0;
-  int i = 10;
-  while (number > i / 10){
-      digit[stat] = (number % i - number % (i / 10))/(i/10);
-      i *= 10;
-      stat++;
-    }
-    
-  for(int i = 0; i < sizeof(digit); i++){
-    lc.setDigit(0,i,digit[i], false);
+
+  digit[0] = number % 10;
+  digit[1] = number / 10 % 10;
+  digit[2] = number / 100 % 10;
+
+  for (int i = 0; i < sizeof(digit); i++) {
+    lc.setDigit(0, i, digit[i], false);
   }
 }
-
-/*void writeNumber(int number){
-  lc.setDigit(0,0,(int)(number/10), false);
-  lc.setDigit(0,1,number-(int)(number/10)*10, false);
-}*/
